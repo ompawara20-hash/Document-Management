7257f0d66a5049fa5aa7fb17cd1171db02776995
Om Pawar <ompawara20@gmail.com>
2026-05-27 23:44:14 +0530

    addead mysql to the setting.py

--- document_mangement/settings.py
+++ document_mangement/settings.py
@@ -76,8 +76,18 @@ WSGI_APPLICATION = 'document_mangement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'document_management_db',
+
+        'USER': 'root',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '3306',
     }
 }
 
