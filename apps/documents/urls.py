ee7410731b84b1c508a598462c6f43275f53cac7
Om Pawar <ompawara20@gmail.com>
2026-05-28 16:21:40 +0530

    creaated an url to upload the document and stores it to media folder and make an entry to database

--- apps/documents/urls.py
+++ apps/documents/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
+from .views import upload_document
+
 
 urlpatterns = [
 
+    path(
+        'upload/',
+        upload_document,
+        name='upload-document'
+    ),
 ]
--- apps/documents/views.py
+++ apps/documents/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from rest_framework.response import Response
+
+from rest_framework import status
+
+from .serializers import DocumentSerializer
+
+
+@api_view(['POST'])
+
+def upload_document(request):
+
+    serializer = DocumentSerializer(
+        data=request.data
+    )
+
+    if serializer.is_valid():
+
+        serializer.save()
+
+        return Response(
+
+            serializer.data,
+
+            status=status.HTTP_201_CREATED
+        )
+
+    return Response(
+
+        serializer.errors,
+
+        status=status.HTTP_400_BAD_REQUEST
+    )
