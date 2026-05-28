ec8ea441f20685cb09fed73e0add923875d77489
Om Pawar <ompawara20@gmail.com>
2026-05-28 14:08:25 +0530

    added Document model

--- apps/documents/models.py
+++ apps/documents/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+
+    title = models.CharField(
+        max_length=255
+    )
+
+    file = models.FileField(
+        upload_to='documents/'
+    )
+
+    file_type = models.CharField(
+        max_length=50
+    )
+
+    related_module = models.CharField(
+        max_length=100
+    )
+
+    related_id = models.IntegerField()
+
+    uploaded_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+
+        return self.title
